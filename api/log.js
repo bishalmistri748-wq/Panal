c286851df2960ce66d25fa51a5b7b339379d9c90
bishalmistri748-wq <bishalmistri748@gmail.com>
2026-08-20 19:23:30 +0530

    Update log.js

--- api/log.js
+++ api/log.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const db = require('../database');
+const router = express.Router();
 
+router.post('/', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const ua = req.headers['user-agent'] || '';
+  db.run(
+    `INSERT INTO activity_log (ip, user_agent) VALUES (?, ?)`,
+    [ip, ua],
+    function(err) {
+      if (err) return res.status(500).send('Log error');
+      res.send('✅ Logged securely.');
+    }
+  );
+});
+
+module.exports = router;
